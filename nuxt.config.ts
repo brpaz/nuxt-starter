6f883ef2999e0227cccbcf8f6cf64d8c8ddf8426
Bruno Paz <184563+brpaz@users.noreply.github.com>
2023-02-04 11:31:21 +0000

    feat: workers deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
+  ssr: true,
   typescript: { strict: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
   nitro: {
